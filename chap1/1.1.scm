c77d986beabe91375b3f0f0344b8917f9fa86649
viodid <david.yunta.aller@gmail.com>
2022-01-09 11:49:05 +0000

    continue SICP

--- chap1/1.1.scm
+++ chap1/1.1.scm
@@ -3,12 +3,37 @@
 ; +---------------+
 (define (twoSquares a b c)
     (cond ((> a b) 
-                (+ (* a a) 
-                (cond (> b c)(* b b) (else * c c))
+                (
+                    + (* a a) 
+                    (cond ((> b c) (* b b)) 
+                        (else (* c c))
+                    )
                 )
             )
-            (else (+ (* b b) (cond (> a c)(* a a) (else * c c)))
+            (
+                else (+ (* b b)
+                (
+                    cond ((> a c)(* a a)) 
+                        (else (* c c))
+                )
+                )
             )
     )
 )
 
+
+; +---------------+
+; |  EXERCISE 1.4 |
+; +---------------+
+; (define (a-plus-abs-b 1 -2)
+;     (if ( > -2 0) + -)
+;         (- 1 -2))
+;           3
+
+
+; +---------------+
+; |  EXERCISE 1.5|
+; +---------------+
+; An applicative-order evaluation interpreter will always enter the infinite recursion, whereas a normal-order 
+; evaluation interpreter will not evaluate the y parameter (the infite recursion) untill the end, so that will meet the first 
+; conditional and return the function before enter into the recursion.
