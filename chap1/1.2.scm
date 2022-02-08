410dfdb062cc7d818431831e078b31d2ea117ded
viodid <david.yunta.aller@gmail.com>
2022-02-08 09:13:13 +0000

    Pascal triangle

--- chap1/1.2.scm
+++ chap1/1.2.scm
@@ -70,3 +70,7 @@
 ; +---------------+
 ; |  EXERCISE 1.12|
 ; +---------------+
+(define (pascal row col)
+    (if (or (= col 1) (= col row)) 1
+    (+ (pascal (- row 1) (- col 1)) (pascal (- row 1) col)))
+)
