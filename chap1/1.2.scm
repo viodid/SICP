d2c7e5355c4e392124d9435d1714493fc575cfd8
viodid <david.yunta.aller@gmail.com>
2022-02-13 12:10:39 +0000

    exercise 1.21

--- chap1/1.2.scm
+++ chap1/1.2.scm
@@ -193,3 +193,27 @@
 (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
 
 ; Using normal-order evaluation, remainder is called 18 times.
+
+
+; +---------------+
+; |  EXERCISE 1.21|
+; +---------------+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+  
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (square x) (* x x))
+
+; (smallest-divisor 199) -> 199
+; (smallest-divisor 1999) -> 1999
+; (smallest-divisor 19999) -> 7
