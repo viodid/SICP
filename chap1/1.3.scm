1a0e30dca4525b19da762d978431b9754458ded6
viodid <david.yunta.aller@gmail.com>
2022-02-19 09:19:47 +0000

    exercise 1.31

--- chap1/1.3.scm
+++ chap1/1.3.scm
@@ -1,4 +1,16 @@
 ; +---------------+
 ; | EXERCISE 2.31 |
 ; +---------------+
-(define (product
+; Product definition
+(define (product term a next b)
+       (if (> a b) 1
+       (* (term a)
+           (product term (next a) next b))))
+
+; Factorial procedure
+ (define (inc n) (+ n 1))
+ (define (nothing n) (* 1 n))
+ (define (factorial a b)
+      (product nothing a inc b))
+; Pi aproximations procedure
+(define (
