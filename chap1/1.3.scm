372e3e831ecd35fe8849b8a742e44068a12fb952
viodid <david.yunta.aller@gmail.com>
2022-02-21 08:13:02 +0000

    exercise 1.32

--- chap1/1.3.scm
+++ chap1/1.3.scm
@@ -40,4 +40,8 @@
 ; Iterative process
 (define (accumulate combiner null-value term a next b)
     (define (iter a result)
-        
+        (if (> a b)
+        result
+            (iter (next a) (combiner result (term a)))))
+    (iter a null-value))
+         
